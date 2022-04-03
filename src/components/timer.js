30e12c8abfff5fd3f5be6fa6063c26962f3e52b8
Kepler-442b <60099921+Kepler-442b@users.noreply.github.com>
2022-04-03 16:05:45 +0900

    remove inner if-conditions in tick function

--- src/components/timer.js
+++ src/components/timer.js
@@ -5,7 +5,7 @@
 
 import React, { useEffect, useState } from "react"
 
-const SECONDS = "00"
+const SECONDS = "02" // TODO: change to 00
 
 const addZeroOnEnd = (int) => int.toString().padStart(2, "0")
 
@@ -24,20 +24,14 @@ const MyTimer = ({
   const [[currMins, currSecs], setTimer] = useState([pomoTime, SECONDS])
   const [[currSBMins, currSBSecs], setSBTimer] = useState([shortBreak, SECONDS])
   const [[currLBMins, currLBSecs], setLBTimer] = useState([longBreak, SECONDS])
-  let pomoTimeFromLocal, sbFromLocal, lbFromLocal, intervalFromLocal
-  // if (typeof window !== "undefined") {
-  //   pomoTimeFromLocal = window.localStorage.getItem("pomoTime")
-  //   sbFromLocal = window.localStorage.getItem("shortBreak")
-  //   lbFromLocal = window.localStorage.getItem("longBreak")
-  //   intervalFromLocal = window.localStorage.getItem("longBreakInterval")
-  // }
+
   useEffect(() => {
-    setTimer([0, SECONDS])
+    setTimer([0, SECONDS]) //TODO: change to pomoTime
     setSBTimer([shortBreak, SECONDS])
     setLBTimer([longBreak, SECONDS])
   }, [pomoTime, shortBreak, longBreak])
 
-  const tick = (id) => {
+  const tick = (id, handleSetTime) => {
     let minsInt, secsInt
     if (isOnShortBreak) {
       minsInt = parseInt(currSBMins)
@@ -50,7 +44,7 @@ const MyTimer = ({
       secsInt = parseInt(currSecs)
     }
 
-    console.log("tick", minsInt, secsInt, isOnShortBreak)
+    // when the timer ends, set the interval to long break, short break, or pomo time
     if (minsInt === 0 && secsInt === 0) {
       clearInterval(id)
       if (isOnShortBreak || isOnLongBreak) {
@@ -60,18 +54,12 @@ const MyTimer = ({
       } else {
         setIsOnShortBreak(true)
       }
+      // when the timer's second reaches 00, set the next second to 59
     } else if (minsInt !== 0 && secsInt === 0) {
-      if (isOnLongBreak)
-        setLBTimer([addZeroOnEnd(minsInt - 1), addZeroOnEnd(59)])
-      else if (isOnShortBreak)
-        setSBTimer([addZeroOnEnd(minsInt - 1), addZeroOnEnd(59)])
-      else setTimer([addZeroOnEnd(minsInt - 1), addZeroOnEnd(59)])
+      handleSetTime([addZeroOnEnd(minsInt - 1), addZeroOnEnd(59)])
+      // otherwise just reduce 1 second from the current time
     } else {
-      if (isOnLongBreak)
-        setLBTimer([addZeroOnEnd(minsInt), addZeroOnEnd(secsInt - 1)])
-      else if (isOnShortBreak)
-        setSBTimer([addZeroOnEnd(minsInt), addZeroOnEnd(secsInt - 1)])
-      else setTimer([addZeroOnEnd(minsInt), addZeroOnEnd(secsInt - 1)])
+      handleSetTime([addZeroOnEnd(minsInt), addZeroOnEnd(secsInt - 1)])
     }
   }
 
@@ -86,7 +74,11 @@ const MyTimer = ({
     if (paused) {
       clearInterval(timerId)
     } else {
-      timerId = setInterval(() => tick(timerId), 1000)
+      timerId = setInterval(() => {
+        if (isOnShortBreak) tick(timerId, setSBTimer)
+        else if (isOnLongBreak) tick(timerId, setLBTimer)
+        else tick(timerId, setTimer)
+      }, 1000)
     }
     return () => clearInterval(timerId)
   }, [[paused, isOnLongBreak, isOnShortBreak]])
