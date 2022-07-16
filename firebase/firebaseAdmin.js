c3c7591895369858093dc4899f4a59e421d3a108
Kepler-442b <60099921+Kepler-442b@users.noreply.github.com>
2022-07-16 21:35:57 +0900

    remove secrets and get vals from env

--- firebase/firebaseAdmin.js
+++ firebase/firebaseAdmin.js
@@ -4,15 +4,48 @@
  */
 
 import * as firebaseAdmin from "firebase-admin/app"
-import secrets from "../secrets.json"
+import {
+  type,
+  project_id,
+  private_key_id,
+  private_key,
+  client_email,
+  client_id,
+  auth_uri,
+  token_uri,
+  auth_provider_x509_cert_url,
+  client_x509_cert_url,
+} from "../prjConfig"
 
-if (!secrets) {
+if (
+  !type ||
+  !project_id ||
+  !private_key_id ||
+  !private_key ||
+  !client_email ||
+  !client_id ||
+  !auth_uri ||
+  !token_uri ||
+  !auth_provider_x509_cert_url ||
+  !client_x509_cert_url
+) {
   console.log(`Failed to load Firebase credentials. `)
 }
 
 if (!firebaseAdmin.apps.length) {
   firebaseAdmin.initializeApp({
-    credential: firebaseAdmin.credential.cert(secrets),
+    credential: firebaseAdmin.credential.cert({
+      type: type,
+      project_id: project_id,
+      private_key_id: private_key_id,
+      private_key: private_key,
+      client_email: client_email,
+      client_id: client_id,
+      auth_uri: auth_uri,
+      token_uri: token_uri,
+      auth_provider_x509_cert_url: auth_provider_x509_cert_url,
+      client_x509_cert_url: client_x509_cert_url,
+    }),
     databaseURL: "https://pomodoro-d9028.firebaseio.com",
   })
 }
---
+++ prjConfig.js
@@ -0,0 +1,16 @@
+/**
+ * File: /prjConfig.js
+ * Copyright (c) 2022 - Sooyeon Kim
+ */
+
+export const type = process.env.type
+export const project_id = process.env.project_id
+export const private_key_id = process.env.private_key_id
+export const private_key = process.env.private_key
+export const client_email = process.env.client_email
+export const client_id = process.env.client_id
+export const auth_uri = process.env.auth_uri
+export const token_uri = process.env.token_uri
+export const auth_provider_x509_cert_url =
+  process.env.auth_provider_x509_cert_url
+export const client_x509_cert_url = process.env.client_x509_cert_url
